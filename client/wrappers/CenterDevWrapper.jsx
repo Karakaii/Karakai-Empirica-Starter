fb5b84c8f119997c0b6d97065bd7173dc74e9f68
Samuel Dupret <samu.dupret@hotmail.com>
2021-05-27 03:30:42 +0100

    add import for centered to centerDevWrapper

--- client/wrappers/CenterDevWrapper.jsx
+++ client/wrappers/CenterDevWrapper.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import DevWrapper from './DevWrapper'
+import { Centered } from "meteor/empirica:core";
 
 export default class CenterDevWrapper extends Component {
     render() {
